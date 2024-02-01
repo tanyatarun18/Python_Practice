0fe3bfa4c70b32c919515ee1b5d8633637902366
Tanya Tarun <112303448+tanyatarun18@users.noreply.github.com>
2024-02-01 23:08:16 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,31 @@
+# 1. Arithmetic operators
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+'''
+# Add
+sum = num1 + num2
+print(sum)
+
+# Subtract
+sub = num1 - num2
+print(sub)
+
+# Multiplication
+mul = num1 * num2
+print(mul)
+
+# Division
+div = num1 / num2
+print(div)
+
+# Modulous
+mod = num1 % num2
+print(mod)
+
+#floor division = means it removes the decimal part of answer and gives the nearest value as answer
+floor = num1 // num2
+print(floor)'''
+
+# Exponential = It gives the answer of power value
+expo = num1 ** num2
+print(expo)

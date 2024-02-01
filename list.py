443cf56fc5012c0a48d2f0157eb7641267284410
Tanya Tarun <112303448+tanyatarun18@users.noreply.github.com>
2024-02-01 23:07:55 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,55 @@
+my_list = ['Tanya', 'Kunal', 'Akanksha', 'Vaibhav', 'Tanisha']
+print(my_list)
+
+# Accessing members in a list
+'''print(my_list[2])
+a = my_list[1]
+print(a)
+
+# Modifying elements in the list
+my_list[0] = 'Tanu'
+print(my_list)
+
+# Append - inserting element in the list(but at last)
+my_list.append('Manish')
+print(my_list)
+
+# Insert element at given position
+my_list.insert(2, 'Manish')
+print(my_list)
+
+# Remove element
+my_list.remove('Vaibhav')
+print(my_list)
+
+# Pop - remove element from given position
+my_list.pop(4)
+print(my_list)
+
+# In - Checking existence of element
+print('Tanu' in my_list)
+
+# Index - finding the index of element
+print(my_list.index("Akanksha"))
+
+# Count - counting the occurence of element (difference between In and Count is that, In gives answer in true or false and Count gives in 0 or 1)
+print(my_list.count('Tanya'))
+
+# Sort - sorting the elents in ascending order
+my_list.sort()
+print(my_list)
+
+# Reverse - reversing the elements
+print(my_list.reverse())
+
+
+# Sort + Reverse in a single line
+my_list.sort(reverse = True)
+print(my_list)
+
+# Slicing the list = It means to take the required part and leave the rest one.
+# First one (1) is starting and second (3) is ending excluding the second  number. these are are the ranges. We use minus to start from last
+print(my_list[1:3])'''
+
+# Copying a list
+print(my_list.copy())
